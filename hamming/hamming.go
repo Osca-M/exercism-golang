7fb95f8d22dbfd3ed5d38a7b5552856d5bdee071
Osca-M <oscamwongera@gmail.com>
2020-12-29 09:00:39 +0300

    hamming excercise final solution

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,32 +1,20 @@
 package hamming
 
 import (
-	"errors"
-	"unicode/utf8"
+	"fmt"
 )
 
 // Distance function to calculate the difference between two strings, or errors
 func Distance(a, b string) (int, error) {
-	var distance int
-	type myError interface {
-		Error() string
-	}
-
-	if utf8.RuneCountInString(a) != utf8.RuneCountInString(b) {
-		defer func() {
-			if r := recover(); r != nil {
-				e := myError(errors.New("panic has occurred"))
-				panic(e)
-			}
-		}()
-		return distance, errors.New("panic in here")
-	}
 	ar, br := []rune(a), []rune(b)
+	if len(ar) != len(br) {
+		return 0, fmt.Errorf("the length of %s is not equal to the length of %s", a, b)
+	}
+	var distance int
 	for i := range ar {
 		if ar[i] != br[i] {
 			distance++
 		}
 	}
-
 	return distance, nil
 }
